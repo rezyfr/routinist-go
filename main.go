b7eb7a6cbb45dcbaea30a4d943238e0ecd57810f
Fidriyanto Rizkillah <fidriyanto.rizkillah@mandirisekuritas.co.id>
2025-05-06 14:18:38 +0700

    🛠️: Load env only on debug mode

--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 	"routinist/internal/app"
 
 	"github.com/joho/godotenv"
-	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+	if os.Getenv("GIN_MODE") != "release" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file: %v", err)
+		}
 	}
 
 	app.Run()
